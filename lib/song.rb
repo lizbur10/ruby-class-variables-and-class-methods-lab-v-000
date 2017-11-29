a994c8683fa06d5c1ff6ce346b252857bd9bc89a
Liz Burton <liz.burton147@gmail.com>
2017-11-29 12:58:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,4 +61,4 @@ end
 
 hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
 lucifer = Song.new("Lucifer", "Jay-Z", "rap" ) 
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap") 
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
